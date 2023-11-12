0b6578bbf5e949dc4bd67e04beb0375d8223b343
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-11-12 12:31:38 +0100

    refactor DirichletCharacter lemmas

--- EulerProducts/RiemannZeta.lean
+++ EulerProducts/RiemannZeta.lean
@@ -9,23 +9,39 @@ import Mathlib.FieldTheory.Finite.Basic
 
 local macro_rules | `($x ^ $y)   => `(HPow.hPow $x $y)
 
-lemma DirichletCharacter.norm_le_one {n : ℕ} (χ : DirichletCharacter ℂ n) (m : ZMod n) :
+section DirichletChar
+
+variable {F : Type} [NormedField F]
+
+lemma ZMod.exists_pos_unit_pow_eq_one : (n : ℕ) → ∃ m : ℕ, 0 < m ∧ ∀ a : (ZMod n)ˣ, a ^ m = 1 :=
+fun
+| 0     => ⟨2, zero_lt_two, Int.units_sq⟩
+| n + 1 => ⟨n.succ.totient, Nat.totient_pos n.succ_pos, ZMod.pow_totient⟩
+
+-- This will eventually show up in Mathlib (future PR by Yaël Dillies)
+lemma pow_eq_one_iff_of_nonneg {R : Type*} [LinearOrderedRing R] {x : R} (hx : 0 ≤ x)
+    {n : ℕ} (hn : n ≠ 0) : x ^ n = 1 ↔ x = 1 := by
+  constructor
+  · intro h
+    exact le_antisymm ((pow_le_one_iff_of_nonneg hx hn).mp h.le)
+      ((one_le_pow_iff_of_nonneg hx hn).mp h.ge)
+  · rintro rfl
+    exact one_pow _
+
+lemma DirichletCharacter.norm_eq_one {n : ℕ} (χ : DirichletCharacter F n) (m : (ZMod n)ˣ) :
+    ‖χ m‖ = 1 := by
+  obtain ⟨e, he₀, he₁⟩ := ZMod.exists_pos_unit_pow_eq_one n
+  refine (pow_eq_one_iff_of_nonneg (norm_nonneg _) he₀.ne').mp ?_
+  rw [← norm_pow, ← map_pow, ← Units.val_pow_eq_pow_val, he₁ m, Units.val_one, map_one, norm_one]
+
+lemma DirichletCharacter.norm_le_one {n : ℕ} (χ : DirichletCharacter F n) (m : ZMod n) :
     ‖χ m‖ ≤ 1 := by
   by_cases h : IsUnit m
-  · match n with
-    | 0 =>
-        rcases Int.isUnit_iff.mp h with rfl | rfl
-        · rw [map_one, norm_one]
-        · refine (sq_le_one_iff <| norm_nonneg _).mp ?_
-          rw [← norm_pow, ← map_pow, neg_one_sq, map_one, norm_one]
-    | n + 1 =>
-      · have H := congr_arg ((↑) : _ → ZMod n.succ) <| ZMod.pow_totient h.unit
-        push_cast at H
-        rw [h.unit_spec.symm]
-        refine (pow_le_one_iff_of_nonneg (norm_nonneg _) (Nat.totient_pos n.succ_pos).ne').mp ?_
-        rw [← norm_pow, ← map_pow, H, map_one, norm_one]
+  · exact (DirichletCharacter.norm_eq_one χ h.unit).le
   · rw [χ.map_nonunit h, norm_zero]
     exact zero_le_one
+
+end DirichletChar
 
 open Complex
 
