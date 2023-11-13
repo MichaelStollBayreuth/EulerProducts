c17d8344a9907dcc73840c6224eff3f3db5a6793
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-11-13 17:49:07 +0100

    bump mathlib

--- EulerProducts/Basic.lean
+++ EulerProducts/Basic.lean
@@ -1,42 +1,6 @@
-import Mathlib.Topology.Algebra.InfiniteSum.Basic
-import Mathlib.Analysis.Normed.Field.Basic
-import Mathlib.Data.Int.Order.Units
-import Mathlib.Data.Nat.Totient
 import Mathlib.FieldTheory.Finite.Basic
 import Mathlib.NumberTheory.DirichletCharacter.Basic
-
-section tsum
-
-open BigOperators
-
-variable {α β : Type*} [AddCommMonoid α] [TopologicalSpace α]
-
--- these should go into `Mathlib.Topology.Algebra.InfiniteSum.Basic`
-open Set in
-lemma tsum_setElem_eq_tsum_setElem_diff [T2Space α] {f : β → α} (s t : Set β)
-    (hf₀ : ∀ b ∈ t, f b = 0) :
-    ∑' a : s, f a = ∑' a : (s \ t : Set β), f a := by
-  simp_rw [tsum_subtype]
-  refine tsum_congr fun b' ↦ ?_
-  by_cases hs : b' ∈ s \ t
-  · rw [indicator_of_mem hs f, indicator_of_mem (mem_of_mem_diff hs) f]
-  · rw [indicator_of_not_mem hs f]
-    rw [mem_diff, not_and, not_not_mem] at hs
-    by_cases h₁ : b' ∈ s
-    · simpa [indicator_of_mem h₁] using hf₀ b' <| hs h₁
-    · exact indicator_of_not_mem h₁ f
-
-open Set in
-/-- If `f b = 0`, then the sum over all `f a` is the same as the sum over `f a` for `a ≠ b`. -/
-lemma tsum_eq_tsum_diff_singleton [T2Space α] {f : β → α} (s : Set β) {b : β} (hf₀ : f b = 0) :
-    ∑' a : s, f a = ∑' a : (s \ {b} : Set β), f a :=
-  tsum_setElem_eq_tsum_setElem_diff s {b} fun _ ha ↦ ha ▸ hf₀
-
-lemma hasSum_singleton (m : β) (f : β → α) : HasSum (({m} : Set β).restrict f) (f m) := by
-  convert_to HasSum (fun x : ({m} : Set β) ↦ f x) (f (⟨m, rfl⟩ : ({m} : Set β)))
-  exact hasSum_single (α := α) _ <| fun m' h ↦ False.elim <| h <| Subtype.ext m'.2
-
-end tsum
+import Mathlib.Analysis.Normed.Field.Basic
 
 section DirichletChar
 
--- EulerProducts/Test.lean
+++ EulerProducts/Test.lean
@@ -1,21 +1,13 @@
 import Mathlib.Tactic
--- import Mathlib.Tactic
+-- import Mathlib.Analysis.PSeries
 
--- lemma pow_eq_one_iff_of_nonneg {R : Type*} [LinearOrderedRing R] (x : R) (hx : 0 ≤ x)
---     (n : ℕ) (hn : n ≠ 0) : x ^ n = 1 ↔ x = 1 := by
---   constructor
---   · intro h
---     exact le_antisymm ((pow_le_one_iff_of_nonneg hx hn).mp h.le)
---       ((one_le_pow_iff_of_nonneg hx hn).mp h.ge)
---   · rintro rfl
---     exact one_pow _
+variable (x : ℝ)
 
--- lemma ZMod.exists_pos_unit_pow_eq_one : (n : ℕ) → ∃ m : ℕ, 0 < m ∧ ∀ a : (ZMod n)ˣ, a ^ m = 1 := by
---   intro n
---   sorry
+#check x ^ 2 -- hover over the `2` in the infoview
+             -- with `import Mathlib.Analysis.PSeries`, it is `2 : ℝ`, else `2 : ℕ`. 
 
-example (a b c d x y : ℝ) (h₁ : x ∈ Set.Ioc a b) (h₂ : y ∈ Set.Ioc c d) :
-    x + y ∈ Set.Ioc (a + c) (b + d) := by
-  refine Set.mem_Ioc.mpr ?_
-  rw [Set.mem_Ioc] at h₁ h₂
-  constructor <;> linarith (config := {splitHypotheses := true})
+-- example (a b c d x y : ℝ) (h₁ : x ∈ Set.Ioc a b) (h₂ : y ∈ Set.Ioc c d) :
+--     x + y ∈ Set.Ioc (a + c) (b + d) := by
+--   refine Set.mem_Ioc.mpr ?_
+--   rw [Set.mem_Ioc] at h₁ h₂
+--   constructor <;> linarith (config := {splitHypotheses := true})
