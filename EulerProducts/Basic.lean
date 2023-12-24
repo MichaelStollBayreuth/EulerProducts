2ab743d7f9bdbdab04bb18d1e3bb5e439fa44ba2
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2023-12-24 23:39:54 +0100

    bump Mathlib, remove Basic.lean

--- EulerProducts/Basic.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.FieldTheory.Finite.Basic
-import Mathlib.NumberTheory.DirichletCharacter.Bounds
-import Mathlib.Analysis.Normed.Field.Basic
-
-section DirichletChar
-
-
-end DirichletChar
