1e5f27beb4130a37d8b59b44de5f1c94e3c89355
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-01-25 19:39:04 +0100

    more ofReal differentiability lemmas

--- EulerProducts/Auxiliary.lean
+++ EulerProducts/Auxiliary.lean
@@ -484,6 +484,9 @@ namespace Complex
 lemma hasDerivAt_ofReal (x : ℝ) : HasDerivAt ofReal' 1 x :=
   HasDerivAt.ofReal_comp <| hasDerivAt_id x
 
+lemma deriv_ofReal (x : ℝ) : deriv ofReal' x = 1 :=
+  (hasDerivAt_ofReal x).deriv
+
 lemma differentiableAt_ofReal (x : ℝ) : DifferentiableAt ℝ ofReal' x :=
   (hasDerivAt_ofReal x).differentiableAt
 
@@ -493,9 +496,13 @@ lemma differentiable_ofReal : Differentiable ℝ ofReal' :=
 
 end Complex
 
-lemma DifferentiableAt.comp_ofReal {e : ℂ → ℂ} {z : ℝ} (hf : DifferentiableAt ℂ e ↑z) :
+lemma DifferentiableAt.comp_ofReal {e : ℂ → ℂ} {z : ℝ} (hf : DifferentiableAt ℂ e z) :
     DifferentiableAt ℝ (fun x : ℝ ↦ e x) z :=
   hf.hasDerivAt.comp_ofReal.differentiableAt
+
+lemma deriv.comp_ofReal {e : ℂ → ℂ} {z : ℝ} (hf : DifferentiableAt ℂ e z) :
+    deriv (fun x : ℝ ↦ e x) z = deriv e z :=
+  hf.hasDerivAt.comp_ofReal.deriv
 
 lemma Differentiable.comp_ofReal {e : ℂ → ℂ} (h : Differentiable ℂ e) :
     Differentiable ℝ (fun x : ℝ ↦ e x) :=
@@ -508,6 +515,38 @@ lemma DifferentiableAt.ofReal_comp {z : ℝ} {f : ℝ → ℝ} (hf : Differentia
 lemma Differentiable.ofReal_comp {f : ℝ → ℝ} (hf : Differentiable ℝ f) :
     Differentiable ℝ (fun (y : ℝ) => (f y : ℂ)) :=
   fun _ ↦ hf.differentiableAt.ofReal_comp
+
+open Complex ContinuousLinearMap in
+lemma HasDerivAt.of_hasDerivAt_ofReal_comp {z : ℝ} {f : ℝ → ℝ} {u : ℂ}
+    (hf : HasDerivAt (fun y ↦ (f y : ℂ)) u z) :
+    ∃ u' : ℝ, u = u' ∧ HasDerivAt f u' z := by
+  lift u to ℝ
+  · have H := (imClm.hasFDerivAt.comp z hf.hasFDerivAt).hasDerivAt
+    simp only [Function.comp_def, imClm_apply, ofReal_im] at H
+    replace H := H.deriv
+    rwa [eq_comm, deriv_const, comp_apply, imClm_apply, smulRight_apply, one_apply, one_smul] at H
+  refine ⟨u, rfl, ?_⟩
+  convert (reClm.hasFDerivAt.comp z hf.hasFDerivAt).hasDerivAt
+  rw [comp_apply, smulRight_apply, one_apply, one_smul, reClm_apply, ofReal_re]
+
+lemma DifferentiableAt.ofReal_comp_iff {z : ℝ} {f : ℝ → ℝ} :
+    DifferentiableAt ℝ (fun (y : ℝ) => (f y : ℂ)) z ↔ DifferentiableAt ℝ f z := by
+  refine ⟨fun H ↦ ?_, ofReal_comp⟩
+  obtain ⟨u, _, hu₂⟩ := H.hasDerivAt.of_hasDerivAt_ofReal_comp
+  exact HasDerivAt.differentiableAt hu₂
+
+lemma Differentiable.ofReal_comp_iff {f : ℝ → ℝ} :
+    Differentiable ℝ (fun (y : ℝ) => (f y : ℂ)) ↔ Differentiable ℝ f :=
+  forall_congr' fun _ ↦ DifferentiableAt.ofReal_comp_iff
+
+lemma deriv.ofReal_comp {z : ℝ} {f : ℝ → ℝ} :
+    deriv (fun (y : ℝ) => (f y : ℂ)) z = deriv f z := by
+  by_cases hf : DifferentiableAt ℝ f z
+  · exact hf.hasDerivAt.ofReal_comp.deriv
+  · have hf' := mt DifferentiableAt.ofReal_comp_iff.mp hf
+    rw [deriv_zero_of_not_differentiableAt hf, deriv_zero_of_not_differentiableAt hf',
+      Complex.ofReal_zero]
+
 
 namespace Complex
 
